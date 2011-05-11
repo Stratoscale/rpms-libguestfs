5bf8b7f691546a31887bbc23d936560088e67614
Richard W.M. Jones <rjones@redhat.com>
2011-05-11 19:54:45 +0100

    Bump and rebuild.

---
+++ bump-and-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -
+
+set -e
+
+rpmdev-bumpspec -c "- Bump and rebuild." libguestfs.spec
+git diff ||:
+echo "Press ENTER to commit, push and rebuild."
+read line
+fedpkg commit -m "Bump and rebuild." -p
+fedpkg build
